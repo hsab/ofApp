2dae3377a49e01e050f9fd76d1b3e97847d7329f
hsab <hsab@users.noreply.github.com>
2017-06-02 18:14:48 -0600

    vscode fix

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,10 @@
 int main()
 {
   ofGLFWWindowSettings settings;
-
+  
   auto window = ofCreateWindow(settings);
   auto app = make_shared<ofApp>();
   ofRunApp(window, app);
-
+  
   return ofRunMainLoop();
 }
